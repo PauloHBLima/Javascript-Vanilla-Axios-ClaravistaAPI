684cfd204b490c539eafe5980eaf569d9a4605de
Paulo Henrique <ph.blima30@gmail.com>
2022-10-11 00:14:58 -0300

    Criar/deletar - Axios and Fetch

--- main.js
+++ main.js
@@ -1,34 +1,60 @@
-const url = "http://localhost:3001/navigation/session_id"
+const url = "http://localhost:3001/navigation"
 const newUser = {
-    name: "Goku",
-    email: "goku@email.com",
-    password: "8000",
+    name: "Goku ss7",
+    email: "goku7@email.com",
+    password: "8000"
 }
 
+const formDataJson = JSON.stringify(newUser)
+
 function getUser() {
-    axios.get(url)
+    axios.get(`${url}/users`)
         .then(response => {
             const data = response.data
             renderResults.textContent = JSON.stringify(data)
         })
         .catch(error => console.log(error))
 }
-
 getUser()
+
+/* const config = {
+    body: JSON.stringify(newUser), 
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+async function addNewUser(){
+  try{
+    const response = await fetch(`${url}/register`, config);
+    const data = await response.json();
+    return data;
+  } catch(err) {
+    console.log(error)
+  }
+
+}
+addNewUser() */
+
 
 function addNewUser() {
 
-    axios.post(url, newUser)
+    axios.post(`${url}/register`, formDataJson, {headers: {
+        'Content-Type': 'application/json'
+    }} )
     .then(response => {
-        alert(JSON.stringify(response.data))
+        const data = response.data
+        console.log(response.data)
+        renderResults.textContent = data.results
     })
     .catch(error => console.log(error))
 }
 
-//addNewUser()
+addNewUser()
 
 function deleteUser() {
-    axios.delete(`${url}/6343843702328c9c67842a3d`)
+    axios.delete(`${url}/user/6343843702328c9c67842a3d`)
     .then(response => {
         alert(JSON.stringify(response.data))
     })
